99b99c7fa99dfbb37b91992bbe307c8fb50b9ae5
Hubert Smusz <hubert.smusz@gmail.com>
2024-05-27 10:40:02 +0200

    Update Container.php

--- Container.php
+++ Container.php
@@ -6,4 +6,35 @@ use Illuminate\Container\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
+     private array $terminatingCallbacks;
+
+    /**
+     * Register a terminating callback with the application.
+     *
+     * @param callable|string $callback
+     *
+     * @return $this
+     */
+    public function terminating($callback)
+    {
+        $this->terminatingCallbacks[] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Terminate the application.
+     *
+     * @return void
+     */
+    public function terminate()
+    {
+        $index = 0;
+
+        while ($index < count($this->terminatingCallbacks)) {
+            $this->call($this->terminatingCallbacks[$index]);
+
+            $index++;
+        }
+    }
 }
